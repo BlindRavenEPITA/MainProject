e63602b0b8ff6130176b4678d51f7d8a34715497
Natsukii <natachapadova@gmail.com>
2017-03-01 22:34:04 +0100

    Add files via upload

---
+++ LineOfSight.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineOfSight : MonoBehaviour {
+    RaycastHit hit;
+    public GameObject player;
+    public Transform ennemy;
+    Vector3 Direction;
+    // Use this for initialization
+    void Start()
+    {
+        Direction = player.transform.position - transform.position;
+    }
+
+    private bool See()
+    {
+        if (Vector3.Angle(Direction,transform.forward) <= 360*0.5f)
+        {
+            if (Physics.Raycast(transform.position,Direction, out hit,10))
+            {
+                return (hit.transform.CompareTag("Player"));
+            }
+        }
+        return false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Physics.Raycast(transform.position, Direction, out hit))
+        {
+            if (hit.transform == player.transform)
+            {
+                //seen
+                Debug.Log("Seen");
+                ennemy.transform.LookAt(player.transform);
+                ennemy.transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+            }
+            else
+            {
+                Debug.Log("not");
+                //not seen
+            }
+        }
+
+
+
+    }
+}
---
+++ LookTp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookTp : MonoBehaviour {
+
+    public float TpCool = 10;
+    public float Timer;
+    private RaycastHit Last;
+    public AudioClip SoundTp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    private GameObject Looking()
+    {
+
+        Vector3 origin = transform.position;
+        Vector3 direction = Camera.main.transform.forward;
+        float range = 1000;
+        if (Physics.Raycast(origin, direction, out Last, range))
+            return Last.collider.gameObject;
+        else
+            return null;
+    }
+
+    private void TpToLook()
+    {
+        transform.position = Last.point + Last.normal*2;
+        if (SoundTp != null)
+            AudioSource.PlayClipAtPoint(SoundTp, transform.position);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Timer < 0)
+            Timer = 0;
+        if (Timer > 0)
+            Timer -= Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && Timer == 0)
+            if (Looking() != null)
+            {
+                Timer = TpCool;
+                TpToLook();
+            }
+	
+	}
+}
