7c8fa1c1295a9d3dd99f4e3f1ef5025b5748fcd2
ThibautBENEFICE <thibaut.benefice@epita.fr>
2017-03-03 00:04:26 +0100

    Add files via upload

---
+++ Interactions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interactions : MonoBehaviour {
+	public GameObject Character;
+	public GameObject[] Interactables;
+	public Vector3 interactSphere;
+	public float Radius;
+
+
+	// Use this for initialization
+	void Start () {
+		Character = GameObject.FindGameObjectWithTag ("Player");
+		Interactables = GameObject.FindGameObjectsWithTag ("Interactable");
+		interactSphere = (Character.transform.position);
+		Radius = 10.0f;
+	}
+	
+	void InteractionPossible(){
+		
+		Collider[] hitColliders = Physics.OverlapSphere(interactSphere, Radius);
+		foreach (Collider Inter in hitColliders) {
+			if (Inter.gameObject.tag == "Interactable") {
+				SceneManager.LoadScene("LevelComplete");
+			}
+		}
+	}
+}
