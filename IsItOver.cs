912addf3e9b50c21c5bf1a164f2c81d15774280a
Natsukii <natachapadova@gmail.com>
2017-03-02 10:24:05 +0100

    Add files via upload

---
+++ IsItOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IsItOver : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject == player)
+            SceneManager.LoadScene("gameOver");
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
---
+++ gameOverScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class gameOverScript : MonoBehaviour {
+    public Texture gameOverT;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), gameOverT);
+        if (GUI.Button(new Rect(Screen.width/2,Screen.height/2,150,25),"Try again?"))
+        {
+            SceneManager.LoadScene("CraveSoutenance1");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 + 25, 150, 25),"I'm over it"))
+        {
+            Application.Quit();
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
