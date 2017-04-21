bd20db35ed52f51b354eab81bdb797023800814f
Célian <celian.gossec@epita.fr>
2017-04-21 13:23:27 +0200

    Submit for PRE_S2_REUNION
    I might needa go now.

---
+++ Dialogue.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour
+{
+	public Canvas GUICanvas;
+	public Transform Player;
+
+	void Start()
+	{
+		GUICanvas.gameObject.SetActive (false);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.collider.tag == "Player")
+			GUICanvas.gameObject.SetActive(true);
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.collider.tag == "Player")
+			GUICanvas.gameObject.SetActive (false);
+	}
+
+	void Update()
+	{
+		//GUICanvas.transform.LookAt (Player);
+		Vector3 difference = Player.transform.position - GUICanvas.transform.position;
+		float rotationY = Mathf.Atan2(difference.x, difference.z) * Mathf.Rad2Deg;
+		GUICanvas.transform.rotation = Quaternion.Euler(0.0f,rotationY , 0.0f);		
+	}
+}
---
+++ getOutlined.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getOutlined : MonoBehaviour {
+	private Shader outline;
+	private Shader normal;
+	private Renderer rend;
+	private GameObject Player;
+	private RaycastHit hit;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		outline = Shader.Find("Custom/NewSurfaceShader");
+		normal = Shader.Find("Standard");
+		Player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics.Raycast (Player.transform.position, Player.transform.TransformDirection (Vector3.forward),out hit, 10f)) 
+		{
+			if (hit.collider.gameObject == this.gameObject)
+				rend.material.shader = outline;
+			else
+				rend.material.shader = normal;
+		}
+		else
+			rend.material.shader = normal;
+	}
+}
