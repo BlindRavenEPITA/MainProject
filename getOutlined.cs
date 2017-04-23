027543420b014214cba2ebdf51c536eb7ff90497
ThibautBENEFICE <thibaut.benefice@epita.fr>
2017-04-23 16:40:02 +0200

    Update getOutlined.cs

--- getOutlined.cs
+++ getOutlined.cs
@@ -1,30 +1,57 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class getOutlined : MonoBehaviour {
 	private Shader outline;
 	private Shader normal;
 	private Renderer rend;
-	private GameObject Player;
+	public GameObject Player;
 	private RaycastHit hit;
+	float visible = 2.0f;
+
 	// Use this for initialization
 	void Start () {
-		rend = GetComponent<Renderer>();
+		try
+		{
+			rend= transform.GetChild (0).transform.GetChild (6).GetComponent<Renderer>();
+		}
+		catch
+		{
+			rend = GetComponent<Renderer>();
+		}
+
 		outline = Shader.Find("Custom/NewSurfaceShader");
 		normal = Shader.Find("Standard");
-		Player = GameObject.FindWithTag ("Player");
+		rend.material.shader = normal;
+
+		//Player = GameObject.FindWithTag ("Player");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Physics.Raycast (Player.transform.position, Player.transform.TransformDirection (Vector3.forward),out hit, 10f)) 
-		{
-			if (hit.collider.gameObject == this.gameObject)
+
+		if (Physics.Raycast (Player.transform.position, Player.transform.TransformDirection (Vector3.forward), out hit, 15.0f)) {
+
+			if (hit.collider.gameObject == gameObject) {
 				rend.material.shader = outline;
-			else
+				visible = 2.0f;
+			} 
+
+			else if (visible > 0)
+				visible -= Time.deltaTime;
+
+			else {
 				rend.material.shader = normal;
-		}
-		else
+				visible = 0;
+			}
+		} 
+
+		else if (visible > 0)
+			visible -= Time.deltaTime;
+
+		else {
+			visible = 0;
 			rend.material.shader = normal;
+		}
 	}
 }
