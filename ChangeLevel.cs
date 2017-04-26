8e27e6b86e97763b35ed1e68178abfdc89cb7257
Célian <celian.gossec@epita.fr>
2017-04-26 03:56:29 +0200

    updated ChangeLevel.cs
    We need sthing working to get into the Warehouse.
    Also, we need to change the player of the GloomyHallway scene : right
    now, the player would become a FPS Capsule upon entering.
    
    Fading dreams NeverForget

--- ChangeLevel.cs
+++ ChangeLevel.cs
@@ -6,19 +6,19 @@ public class ChangeLevel : MonoBehaviour
 {
     public string Destination;
 
-    IEnumerator FadeToDestinationLevel()
+    /*IEnumerator FadeToDestinationLevel()
     {
         float fadeTime = GameObject.FindGameObjectWithTag("Player").GetComponent<Fading>().BeginFade(1);
         yield return new WaitForSeconds(fadeTime);
         SceneManager.LoadScene(Destination);
-    }
+    }RIP ALL MY FADING DREAMS*/
 
 
     void OnCollisionEnter(Collider player)
     {
         if (player.tag == "Player")
         {
-            StartCoroutine(FadeToDestinationLevel());
+			SceneManager.LoadScene(Destination);
         }
     }
 }
